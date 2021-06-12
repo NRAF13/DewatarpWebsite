20012f1ed5889b7860558b133685ce22a8965195
NaufalRafif <77334971+NRAF13@users.noreply.github.com>
2021-06-12 17:52:21 +0700

    Add files via upload

---
+++ donasi/index.php
@@ -0,0 +1,255 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width,initial-scale=1.0">
+<title>Donasi - DEWATA ROLEPLAY INDONESIA</title>
+<meta name="description" content="DEWATA ROLEPLAY merupakan komunitas game online dari GTA SA modifikask untuk bermain roleplay">
+<meta name="keywords" content="dewatarp, dewata roleplay, dewata">
+<link rel="shortcut icon" href="../favicon.ico">
+
+<meta property="og:site_name" content="DEWATA ROLEPLAY">
+<meta property="og:url" content="http://dewatarp.xyz/">
+<meta property="og:site_name" content="DEWATA ROLEPLAY">
+<meta property="og:type" content="webiste">
+<meta property="og:title" content="DEWATA ROLEPLAY INDONESIA">
+<meta property="og:description" content="DEWATA ROLEPLAY INDONESIA merupakan komunitas game online dari GTA SA modifikasi untuk bermain roleplay">
+<meta property="og:image" content="https://dewatarp.xyz/DEWATA.png">
+
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+<link rel="stylesheet" href="../css/bootstrap.css">
+<link rel="stylesheet" href="../css/style.css">
+</head>
+
+<body>
+<header>
+<div class="container">
+<div class="row">
+
+<nav class="navbar navbar-expand-md navbar-dark">
+<a class="navbar-brand d-block" href="#"><img src="../img/logodrp.png" alt="" style="max-width:105px;" class="d-inline-block align-text-top"></a>
+<label class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+<span class="fas fa-bars"></span>
+</label>
+<div class="collapse navbar-collapse justify-content-md-end" id="navbarSupportedContent">
+<ul class="navbar-nav">
+<li class="nav-item">
+<a class="nav-link" href="../">BERANDA</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="/forum/" target="_blank">FORUM</a>
+</li>
+</ul>
+</div>
+</nav>
+
+</div>
+</div>
+</div>
+</header>
+
+<div class="container">
+<header class="text-center">
+<h1 class="text-capitalize">
+
+<span class="d-block wow fadeInUp" data-wow-delay="1.0s">Donasi & Benefit</span>
+</h1>
+</header>
+<p class="wow fadeInLeft" data-wow-delay="2.8s">TIM ADMIN DAN SEGENAP WARGA DEWATA MENGUCAPKAN BANYAK TERIMA KASIH KEPADA PARA DONATUR YANG SUDAH MEMBANTU SERVER DEWATARP TETAP EKSIS DAN BERKEMBANG, KAMI TETAP MENERIMANDONASI DALAM BENTUK APAPUN DAN DARI SIAPAPUN UNTUK PERBAIKAN PERKEMBANGAN SERVER KEDEPANNYA.</p>
+<p class="wow fadeInLeft" data-wow-delay="2.8s">
+DONASI BERSIFAT SUKARELA TANPA PAKSAAN!!<br>
+- BUKAN PEMBELIAN VIP<br>
+- BUKAN PEMBELIAN ITEM IN GAME<br>
+- BUKAN PEMEBELIAN UANG IN GAME<br>
+TETAPI DONATUR MENDAPATKAN BENEFIT TERTERNTU ATAU SESUAI PERMINTAAN.
+</p>
+<p class="wow fadeInLeft" data-wow-delay="2.8s">DILARANG TRANSAKASI UANG NYATA(RUPIAH) DENGAN ITEM/UANG IN GAME KEPADA PEMAIN LAIN HATI HATI PENIPUAN!!. KAMI TIDAK BERTANGGUNG JAWAB JIKA TERJADI TRANSAKSI UANG NYATA TANPA SEPENGETAHUAN DAN SEIZIN TIM ADMIN DEWATARP.</p>
+<p class="wow fadeInLeft" data-wow-delay="2.8s">LAKUKAN TRANSAKSI HANYA DENGAN TIM ADMIN DAN DEV SERVER DEWATARP YANG TERTERA DI DISCORD DEWATARP: KONTAK DICSORD DEV DEWATARP:<br>
+- NRAF#5614<br>
+<i>*Pastikan kontak disco d sesuai dengan yang diatas!</i></p>
+<p class="wow fadeInLeft" data-wow-delay="2.8s">PEMBAYARAN DONASI HANYA MELALUI TRANSFER KE: BCA 1200556638 a/n NAUFAL RAFIF ARDYANSYAH<br>
+082335112043 TELKOMSEL<br>
+082335112043 GO-PAY a/n NAUFAL RAFIF ARDYANSYAH<br>
+<i>*Hati hati terhadap admin dewatarp yang memberikan benefit dan rekening selain diatas!!</i></p>
+<hr>
+<center><p class="wow fadeInLeft" data-wow-delay="2.8s">BENEFIT DONASI KHUSUS BULANAN <br> INFO DAN UPDATE HANYA MELALUI DISCORD DEWATA ROLEPLAY</p></center>
+<p class="wow fadeInLeft" data-wow-delay="2.8s">
+[1] Benefit diatas berlaku selama 30 Hari.<br>
+[2] Fitur Membership Prime bisa dicek dengan ([PRIME]) di nametag in game.<br>
+[3] Kelebihan PRIME:(1). Rumah diskon 30% selama 1 bulan (2). Kendaraan car dealer diskin 30% selama 1 bulan (3). Akses ke vip chat (4). Biaya asuransi hanya 1% dari harga kendaraan [2% Non Prime].<br>
+[4] Donatur tidak mendapat perlakuan khusus baik dari Dev ataupun Warga, dalam bermain rp semua wajib mengikuti peraturan server yang berlaku.<br>
+[5] Membership PRIME berlaku selama 1 bulan, bonus kendaraan berlaku permanen.<br>
+[6] Proses donasi diinfokan melalui <a href="https://discord.gg/8UQGNfAruN">Discord</a> Dewata Roleplay.
+</p>
+<hr>
+<table>
+<thead>
+<tr>
+<th>Benefit</th>
+<th>Donasi</th>
+<th>Keterangan</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td>Membership</td>
+<td>PM ADMIN</td>
+<td>Diinfokan setiap bulan melalui discord DEWATA ROLEPLAY</td>
+</tr>
+<tr>
+<td>Menyesuaikan</td>
+<td>Sukarela</td>
+<td>Donasi sukarela dengan benefit menyesuaikan</td>
+</tr>
+<tr>
+<td>Cash In Game</td>
+<td>IDR 1000</td>
+<td>Rate IDR 1000 = $1000</td>
+</tr>
+<tr>
+<td>Custom Nomor Telepon</td>
+<td>IDR 5000</td>
+<td>Custom Nomor Telepon 6 Digit Angka</td>
+</tr>
+<tr>
+<td>Custom Plat Nomor</td>
+<td>IDR 10000</td>
+<td>Custom Plat Nomor 6 karakter</td>
+</tr>
+<tr>
+<td></td><td></td><td></td>
+</tr>
+<tr>
+<td>Kendaraan car dealer #1</td>
+<td>IDR 10000</td>
+<td>Kendaraan car dealer dengan harga maksimal 1jt</td>
+</tr>
+<tr>
+<td>Kendaraan car dealer #2</td>
+<td>IDR 20000</td>
+<td>Kendaraan car dealer dengan harga maksimal 2jt</td>
+</tr>
+<tr>
+<td>Kendaraan car dealer #3</td>
+<td>IDR 30000</td>
+<td>Kendaraan car dealer dengan harga maksimal 3jt</td>
+</tr>
+<tr>
+<td>Kendaraan car dealer #4</td>
+<td>IDR 40000</td>
+<td>Kendaraan car dealer dengan harga maksimal 4jt</td>
+</tr>
+<tr>
+<td>Kendaraan car dealer #5</td>
+<td>IDR 50000</td>
+<td>Kendaraan car dealer dengan harga maksimal 5jt</td>
+</tr>
+<tr>
+<td>Kendaraan car dealer #6</td>
+<td>IDR 60000</td>
+<td>Kendaraan car dealer dengan harga maksimal 6jt</td>
+</tr>
+<tr>
+<td>Kendaraan car dealer #7</td>
+<td>IDR 70000</td>
+<td>Kendaraan car dealer dengan harga maksimal 7jt</td>
+</tr>
+<tr>
+<td>Kendaraan car dealer #8</td>
+<td>IDR 80000</td>
+<td>Kendaraan car dealer dengan harga maksimal 8jt</td>
+</tr>
+<tr>
+<td>Kendaraan car dealer #9</td>
+<td>IDR 90000</td>
+<td>Kendaraan car dealer dengan harga maksimal 9jt</td>
+</tr>
+<tr>
+<td>Kendaraan car dealer #10</td>
+<td>IDR 100000</td>
+<td>Kendaraan car dealer dengan harga maksimal 110jt</td>
+</tr>
+<tr>
+<td></td><td></td><td></td>
+</tr>
+<tr>
+<td>Rumah Eksklusif</td>
+<td>IDR 20000(Kelas Ultra)</td>
+<td>Rumah Eksklusif Kelas Ultra Sudah Lengkap Dengan Garasi</td>
+</tr>
+<tr>
+<td>Custom Mapping</td>
+<td>IDR 2000/Object</td>
+<td>Custom Mapping dengan maksimal 30 Object</td>
+</tr>
+<tr>
+<td>Paket Bundling</td>
+<td>Max 6 Benefit</td>
+<td>6 Benefit donasi diskon 30%</td>
+</tr>
+</tbody>
+</table><br>
+<p class="wow fadeInLeft" data-wow-delay="2.8s">
+[1] Benefit kendaraan berupa gift dikirim 1x ke donatur setelah itu haral dijaga dengan baik.<br>
+[2] Kendaraan tidak memiliki imunitas dan perlakuan khusus.<br>
+[3] *Donatur tidak mendapat perlakuan khusus baim dari dev ataupun dari warga biasa, semua tetap wajib mengikuti peraturan server yang ada dalam bermain roleplay.<br>
+[4] Donasi bersifat sukarela untuk mendukung DEWATA ROLEPLAY.(BUKAN PEMBELIAN ITEM IN GAME)
+</p>
+<hr>
+<header class="text-center mb-14 mb-lg-20">
+<h2 class="text-capitalize mb-4 mb-lg-7">
+
+<span class="d-block wow fadeInUp" data-wow-delay="2.5s">FORMAT DONASI</span>
+</h2>
+</header>
+
+<p class="wow fadeInLeft" data-wow-delay="2.8s">
+PEMBAYARAN DONASI HANYA MELALUI TRANSFER KE: BCA 1200556638 a/n NAUFAL RAFIF ARDYANSYAH<br>082335122043 TELKOMSEL<br>GO-PAY 08233511203 a/n NAUFAL RAFIF ARDYANSYAH<br>
+Setelah tranfer harap kirim format berikut melalui PM discord tim DEWATA ROLEPLAY:<br>
+NAMA IC:<br>
+BENEFIT:<br>
+NAMA REKENING PENGIRIM:<br>
+BUKTI TRANSFER:(Screenshot)
+</p>
+<p class="wow fadeInLeft" data-wow-delay="2.8s">
+<i>
+*Hati hati terhadap Admin / Pihak yang mengaku admin Dewata Roleplay yang memberikan rekening dan benefit selain diatas, harap segera batalkan donasi!!
+*Jangan ragu dan sungkan untuk konsultasi terkait donasi dengan Tim Admin
+</i>
+</p>
+</div>
+
+<footer class="wow fadeIn">
+<hr>
+<div class="container">
+<div class="row">
+
+<div class="mb-7 mb-lg-12">
+<a href="/"><img src="../img/logodrp.png" class="img-fluid" alt="dewatarp"></a>
+</div>
+<p>Merupakan komunitas game online dari GTA SA modifikasi untuk bermain roleplay.</p>
+<div class="d-flex flex-wrap mx-n1 mx-lg-n3 pt-4 pt-lg-9">
+<a class="btn btnUngu rounded-circle position-relative mx-1 mx-lg-3" href="https://discord.gg/8UQGNfAruN"><i class="fab fa-discord"></i></a> <a class="btn btnUngu rounded-circle position-relative mx-1 mx-lg-3 text-capitalize" href="https://www.facebook.com/dewatarp" target="_blank"><i class="fab fa-facebook"></i></a>
+</div>
+
+
+</div>
+</div>
+<br>
+</div>
+
+<div class="tengah">
+<p>© DEWATA ROLEPLAY 2021</p>
+</div>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+<script type="text/javascript" src="../js/jquery.js"></script>
+<script src="../js/wow.js"></script>
+<script src="../js/nraf.js"></script>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width,initial-scale=1.0">
+<title>DEWATA ROLEPLAY INDONESIA</title>
+<meta name="description" content="DEWATA ROLEPLAY merupakan komunitas game online dari GTA SA modifikask untuk bermain roleplay">
+<meta name="keywords" content="dewatarp, dewata roleplay, dewata">
+<link rel="shortcut icon" href="favicon.ico">
+
+<meta property="og:site_name" content="DEWATA ROLEPLAY">
+<meta property="og:url" content="http://dewatarp.xyz/">
+<meta property="og:site_name" content="DEWATA ROLEPLAY">
+<meta property="og:type" content="webiste">
+<meta property="og:title" content="DEWATA ROLEPLAY INDONESIA">
+<meta property="og:description" content="DEWATA ROLEPLAY INDONESIA merupakan komunitas game online dari GTA SA modifikasi untuk bermain roleplay">
+<meta property="og:image" content="https://dewatarp.xyz/DEWATA.png">
+<script type="33d3efcb5d6fc148d665a57e-text/javascript">
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', 'G-E24YQZT51M');
+    </script>
+
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+<link rel="stylesheet" href="css/bootstrap.css">
+<link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+<header>
+<div class="container">
+<div class="position-relative">
+<div class="row">
+
+<nav class="navbar navbar-expand-md navbar-dark">
+<a href="#"><img src="img/logodrp.png" alt="" style="max-width:105px;" class="d-inline-block align-text-top"></a>
+<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+<span class="fas fa-bars"></span>
+</button>
+<div class="collapse navbar-collapse justify-content-md-end" id="navbarSupportedContent">
+<ul class="navbar-nav">
+<li class="nav-item">
+<a class="nav-link" href="#beranda">BERANDA</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="#status">STATISTIK</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="#petunjuk">PETUNJUK</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="https://forum.dewatarp.xyz/" target="_blank">FORUM</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="/donasi/" target="_blank">DONASI</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="https://status.dewatarp.xyz/" target="_blank">STATUS</a>
+</li>
+</ul>
+</div>
+</nav>
+
+</div>
+</div>
+</div>
+</header>
+
+<div id="beranda">
+<div class="container">
+<div class="row">
+<div class="position-relative">
+
+<h1 class="text-capitalize mb-3 mb-md-5">
+<span class="d-block wow fadeInLeft" data-wow-delay="2.5s">DEWATA <span class="text-white">ROLEPLAY</span></span>
+</h1>
+<p class="wow fadeInLeft" data-wow-delay="2.8s">Merupakan komunitas game online dari GTA SA modifikasi untuk bermain roleplay.</p>
+<p class="wow fadeInLeft" data-wow-delay="2.8s">ROLEPLAY merupakan permainan memerenkan sebuah karakter seperti layaknya di dunia nyata dengan cerita serta kehidupan yang kita tentulan sendiri, seperti di dunia nyata hidup kita kadang membosankan, terkadang hidup kita juga penuh drama.</p>
+<p class="wow fadeInLeft" data-wow-delay="2.8s">ROLEPLAY Merupakan permaianan fiktif dan tidak ada hubungannya sama sekali dengan apa yang terjadi di dunia nyata. Permainan ininadalah seni merangkai kreativitas cerita.</p>
+
+<div class="d-flex flex-wrap mx-n1 mx-lg-n3 pt-4 pt-lg-9">
+<a class="btn btnWhite position-relative mx-1 mx-lg-3 wow bounceIn" data-wow-delay="3s" href="https://discord.gg/8UQGNfAruN" target="_blank">DISCORD</a> <a class="btn btnUngu position-relative mx-1 mx-lg-3 text-capitalize wow bounceIn" data-wow-delay="3s" href="dl/DewataRP.exe">Launcher</a>
+</div>
+
+</div>
+</div>
+</div>
+<div class="wow fadeInLeft" data-wow-delay="3s">
+<img src="DEWATA.png" class="img-fluid" alt="dewatarp">
+</div>
+</div>
+
+<div id="status" class="bgItem">
+<div class="container">
+<div class="row">
+
+<h2 class="text-capitalize mb-4 mb-lg-7">
+
+<span class="d-block mb-3 wow fadeInRight" data-wow-delay="0.9s">Status Update</span>
+</h2>
+<p class="wow fadeInLeft" data-wow-delay="2.8s">DEWATA ROLEPLAY: 0 PLAYERS</p>
+
+</div>
+</div>
+</div>
+
+<div id="petunjuk">
+<div class="container">
+<div class="row">
+<div class="justify-content-center">
+
+<h1 class="d-block wow fadeInLeft" data-wow-delay="2.5s">System Requirementss</h1>
+
+<div class="card bgItem wow fadeInUp" data-wow-delay="2.8s">
+<img src="DEWATA.png" class="card-img-top" alt="dewatarp">
+<div class="card-body">
+<h5 class="card-title">Minimum</h5>
+<p class="card-text">CPU: 1Ghz Pentium III or AMD Athlon Processor<br>
+RAM: 256MB<br>
+GPU: Geforce 3 Keatas</p>
+</div>
+</div>
+<br>
+<div class="card bgItem wow fadeInUp" data-wow-delay="2.8s">
+<img src="DEWATA.png" class="card-img-top" alt="dewatarp">
+<div class="card-body">
+<h5 class="card-title">Recommended</h5>
+<p class="card-text">CPU: Intel Pentium 4 or AMD Athlon XP Processor<br>
+RAM: 384MB<br>
+GPU: Geforce 6 recommended</p>
+</div>
+</div>
+
+</div>
+</div>
+</div>
+</div><br>
+<br>
+
+<footer>
+<div class="bgItem">
+<div class="container wow fadeIn">
+<div class="row">
+
+<div class="mb-7 mb-lg-12">
+<a href="/"><img src="img/logodrp.png" class="img-fluid" alt="dewatarp"></a>
+</div>
+<p>Merupakan komunitas game online dari GTA SA modifikasi untuk bermain roleplay.</p>
+<div class="d-flex flex-wrap mx-n1 mx-lg-n3 pt-4 pt-lg-9">
+<a class="btn btnUngu rounded-circle position-relative mx-1 mx-lg-3" href="https://discord.gg/8UQGNfAruN"><i class="fab fa-discord"></i></a> <a class="btn btnUngu rounded-circle position-relative mx-1 mx-lg-3 text-capitalize" href="https://www.facebook.com/dewatarp" target="_blank"><i class="fab fa-facebook"></i></a>
+</div>
+
+
+</div>
+</div>
+<br>
+</div>
+
+<div class="tengah">
+<p>© DEWATA ROLEPLAY 2021</p>
+</div>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+<script type="text/javascript" src="js/jquery.js"></script>
+<script src="js/wow.js"></script>
+<script src="js/nraf.js"></script>
+
+</body>
+</html>
